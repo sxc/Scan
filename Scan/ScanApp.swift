4d8a1b0e376dd7d9f44d6ff8a1d4d06588e8bfaa
sxc <qiuyequ@icloud.com>
2021-04-27 11:16:53 +0800

    Initial Commit

---
+++ Scan/ScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanApp.swift
+//  Scan
+//
+//  Created by Xiaochun Shen on 2021/4/27.
+//
+
+import SwiftUI
+
+@main
+struct ScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
